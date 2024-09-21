de6bff0427d5c51fe8f14e3ae98a76f441b5305d
EC2 Default User <ec2-user@ip-172-31-7-62.ap-northeast-1.compute.internal>
2024-09-21 08:37:24 +0000

    Finish2 Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "16d0c480ed6e41de875998bd14775670.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+     
+  get '/users/:id/confirm' => 'users#confirm', as: 'confirm'
+  
+ patch '/users/:id/confirms' => 'users#confirms', as: 'confirms'
+devise_for :users
+root to: 'homes#top'
+   resources :users, only: [:show, :edit, :create, :index, :update, :destroy] 
+   resources :posts, only: [ :edit, :create, :index, :destroy] 
+   resources :comments, only: [:show,  :index]
+   resources :groups, only: [:create, :index, :destroy] 
+   resources :chats, only: [ :create, :index, :destroy] 
 end
