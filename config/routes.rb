367291eea576ae8d922f7e6a673e879315fe79be
EC2 Default User <ec2-user@ip-172-31-7-62.ap-northeast-1.compute.internal>
2024-09-22 09:25:18 +0000

    flash Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-     
+
   get '/users/:id/confirm' => 'users#confirm', as: 'confirm'
-  
+
  patch '/users/:id/confirms' => 'users#confirms', as: 'confirms'
 devise_for :users
 root to: 'homes#top'
-   resources :users, only: [:show, :edit, :create, :index, :update, :destroy] 
-   resources :posts, only: [ :edit, :create, :index, :destroy] 
+   resources :users, only: [:show, :edit, :create, :index, :update, :destroy]
+   resources :posts, only: [ :edit, :create, :index, :destroy]
    resources :comments, only: [:show,  :index]
-   resources :groups, only: [:create, :index, :destroy] 
-   resources :chats, only: [ :create, :index, :destroy] 
+   resources :groups, only: [:create, :index, :destroy]
+   resources :chats, only: [ :create, :index, :destroy]
 end
